0242d0644f49714f671d6e6e90438b023e9140a6
jamesbwn <raxioma@outlook.com>
2022-11-13 02:10:58 +1000

    stat loan contract

--- indexLocal.js
+++ indexLocal.js
@@ -1,4 +1,3 @@
-const express = require("express")
 require('dotenv').config()
 const ethers = require('ethers');
 const loanABI = require('./loan.json')
@@ -6,7 +5,7 @@ const loanABI = require('./loan.json')
 const WSS = "https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"		// Maninet RPC
 var provider = new ethers.providers.JsonRpcProvider(WSS);
 
-const loanAddress = '0x0828dCadB0f32a6757B00aA18B753056AA51200A';			// Mainnet Loan
+const loanAddress = '0x8D9E65fc5560d20fC97B2b8fc6b732A51732bf5C';			// Mainnet Loan
 
 const opKey = process.env.privatekey
 const walletForControl = new ethers.Wallet(opKey, provider); //Loan Operator
@@ -17,16 +16,13 @@ const loanContract = new ethers.Contract(
 	walletForControl
 )
 
-const port = 3000;
-
-const app = express();
-
 const main = async () => {
 	try {
 		const collateralCount = await loanContract.getCollateralLen()
 		const isSwap = await loanContract.isSwappable()
 		console.log('loanEther:: swappable ', isSwap, collateralCount);
 		if(collateralCount > 0 && isSwap) {
+			console.log('loanEther:: start')
 			await loanContract.swapAssets();
 			console.log('loanEther:: swapAssets');
 		}
@@ -35,16 +31,4 @@ const main = async () => {
 	}
 }
 
-app.listen(process.env.PORT || 3000, () => {
-	console.log(`App start on port ${port}`);
-});
-
-app.get('/loanEther', async function (req, res) {
-
-	main()
-	res.send("loanEther:: success")
-})
-
-app.get('/', async function (req, res) {
-	res.send("loanEther:: op request")
-})
+main()
